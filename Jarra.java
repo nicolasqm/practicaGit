5a8ade58c1868b8d4e8b21a581f6209a11226523
pacoarjonaa <pacoarjonaj@uma.es>
2020-03-26 11:44:17 +0100

    VersionPaco

--- Jarra.java
+++ Jarra.java
@@ -30,4 +30,19 @@ public class Jarra {//variables de instancia
     publicString toString(){
         return ("("+this.capacidad+","+this.cantidad+")");
 	}
+
+	public Object clone() throws CloneNotSupportedException {
+		return (Jarra) super.clone();
+	}
+	
+	public bool equals(Jarra otraJarra){
+	bool eqs = false;
+	if (null == otraJarra){ eqs = false; }
+	else if (this == otraJarra){ eqs = true; }
+	else if (otraJarra instanceOf Jarra.class){
+		eqs = this.capacidad == otraJarra.capacidad &&
+		this.cantidad == otraJarra.cantidad;
+	} else { eqs = false; }
+	return eqs;
+	}
 }
